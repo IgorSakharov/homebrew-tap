d749e96174189e8890c89a76dae2738e7d30a635
Ihor Sakharov <ihor.sakharov@pixieset.com>
2025-07-21 12:14:29 -0700

    Add container-selector formula correct address

--- Formula/container-selector.rb
+++ Formula/container-selector.rb
@@ -1,7 +1,7 @@
 class ContainerSelector < Formula
   desc "Interactive Docker container selector and command executor"
   homepage "https://github.com/IgorSakharov/container-selector"
-  url "https://github.com/IgorSakharov/container-selector/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/IgorSakharov/contaner-selector/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "sha256:f487c175c6dbffaeb1c061e5a56e9e9e63479e591c8b45092ebc35eddd8f1165"  # Replace with actual hash
   license "MIT"
   version "1.0.1"
